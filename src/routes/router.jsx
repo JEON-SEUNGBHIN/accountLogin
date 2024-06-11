60cfdf45293265b7b1de708dba16f9d3468530b7
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-06-11 18:20:53 +0900

    add: home 경로 추가

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import Home from "../pages/Home";
 // 기타 필요한 컴포넌트 임포트
 
 const router = createBrowserRouter([
   {
-    path: "/login",
+    path: "/",
     element: <Login />,
   },
   {
     path: "/signup",
     element: <SignUp />
+  },
+  {
+    path: "/home",
+    element: <Home />
   },
   // 기타 라우트
 ]);
