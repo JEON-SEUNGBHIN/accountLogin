3e1b54064c6a18e67b275199ad846db67206c7b9
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-06-11 15:52:16 +0900

    🎉 add: 프로젝트 파일 생성

---
+++ src/components/Home/Home.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const InStyledBox = styled.div`
+  width: 60%;
+  background-color: white;
+  margin: 2rem auto;
+  border: 1px solid white;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  display: flex;
+`;
