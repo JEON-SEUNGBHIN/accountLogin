dca580537a3244946229d30189679c161edcc2f9
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-06-11 18:23:34 +0900

    update: TanStack Query 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import GlobalStyles from "./components/GlobalStyled";
+import Header from "./layout/Header";
+import router from "./routes/router";
 
 function App() {
   return (
     <>
-      
+      <GlobalStyles />
+      <Header />
+      <RouterProvider router={router}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
